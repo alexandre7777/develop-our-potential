fe1363bc61b3e551bb27f2574ce90e1963e8218a
Alexandre <alexandre.genet7@gmail.com>
2018-08-10 20:25:36 +0200

    add drawer with items TODO navigation

--- app/src/main/java/com/alexandre/potentialgrowth/ui/home/HomeActivity.kt
+++ app/src/main/java/com/alexandre/potentialgrowth/ui/home/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.alexandre.potentialgrowth.ui.home
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.alexandre.potentialgrowth.R
@@ -8,6 +9,12 @@ import kotlinx.android.synthetic.main.activity_home.*
 import android.support.v4.view.ViewPager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.app.FragmentManager
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
 import com.alexandre.potentialgrowth.ui.home.dashboard.DashBoardFragment
 import com.alexandre.potentialgrowth.ui.home.knowledge.KnowledgeFragment
 import com.alexandre.potentialgrowth.ui.home.reward.RewardFragment
@@ -22,11 +29,33 @@ class HomeActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(false)
-
         setupViewPager(viewPager);
 
         tabs.setupWithViewPager(viewPager);
+
+        setupDrawer()
+    }
+
+    private fun setupDrawer() {
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            drawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
+
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
     }
 
     private fun setupViewPager(viewPager: ViewPager) {
@@ -56,6 +85,16 @@ class HomeActivity : AppCompatActivity() {
 
         override fun getPageTitle(position: Int): CharSequence? {
             return mFragmentTitleList.get(position)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
