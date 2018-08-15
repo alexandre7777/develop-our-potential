436777b5f484547072273fc88d273976cdb86e3e
Alexandre <alexandre.genet7@gmail.com>
2018-08-15 11:35:14 +0200

    custom display for your dairy layout

--- app/src/main/java/com/alexandre/potentialgrowth/model/Dairy.kt
+++ app/src/main/java/com/alexandre/potentialgrowth/model/Dairy.kt
@@ -2,6 +2,10 @@ package com.alexandre.potentialgrowth.model
 
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.support.v4.content.ContextCompat
+import com.alexandre.potentialgrowth.R
 import com.google.gson.annotations.SerializedName
 import java.util.*
 
@@ -15,4 +19,35 @@ import java.util.*
 data class Dairy(@PrimaryKey(autoGenerate = true) val idDairy: Long,
                  @field:SerializedName("id_type") val idType: Int,
                  @field:SerializedName("dairy_text") val dairyText: String,
-                 @field:SerializedName("time") val time: Date){}
+                 @field:SerializedName("time") val time: Date){
+
+    public fun getTypeBackgroundDrawable(context : Context) : Drawable? {
+        when (idType){
+            1 -> {
+                return ContextCompat.getDrawable(context, R.drawable.round_blue_background)
+            }
+            2 -> {
+                return ContextCompat.getDrawable(context, R.drawable.round_orange_background)
+            }
+            3 -> {
+                return ContextCompat.getDrawable(context, R.drawable.round_green_background)
+            }
+        }
+        return ContextCompat.getDrawable(context, R.drawable.round_blue_background)
+    }
+
+    public fun getTypeText(context : Context) : String {
+        when (idType){
+            1 -> {
+                return context.getString(R.string.goals)
+            }
+            2 -> {
+                return context.getString(R.string.stories)
+            }
+            3 -> {
+                return context.getString(R.string.ideas)
+            }
+        }
+        return ""
+    }
+}
--- app/src/main/java/com/alexandre/potentialgrowth/ui/yourdairy/DairyViewHolder.kt
+++ app/src/main/java/com/alexandre/potentialgrowth/ui/yourdairy/DairyViewHolder.kt
@@ -7,10 +7,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.alexandre.potentialgrowth.R
 import com.alexandre.potentialgrowth.model.Dairy
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DairyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val dairyTxt: TextView = view.findViewById(R.id.dairyTxt)
+    private val typeTxt: TextView = view.findViewById(R.id.typeTxt)
+    private val dateTxt: TextView = view.findViewById(R.id.dateTxt)
     private var dairy: Dairy? = null
 
     fun bind(dairy: Dairy?) {
@@ -22,7 +26,13 @@ class DairyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private fun showRepoData(dairy: Dairy) {
         this.dairy = dairy
 
-        dairyTxt.setText(dairy.dairyText)
+        dairyTxt.text = dairy.dairyText
+
+        typeTxt.text = dairy.getTypeText(typeTxt.context)
+
+        typeTxt.background = dairy.getTypeBackgroundDrawable(typeTxt.context)
+
+        dateTxt.text =  dateTxt.context.getString(R.string.date_dairy, SimpleDateFormat("MM/dd/yyy", Locale.getDefault()).format(dairy.time))
     }
 
     companion object {
