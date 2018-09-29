5ee29b75158bab5d55d19ea93ef75c0c0b881a24
Alexandre <alexandre.genet7@gmail.com>
2018-09-29 11:51:08 +0200

    redirect to instruction activity if instruction not learned

--- app/src/main/java/com/alexandre/potentialgrowth/ui/home/HomeActivity.kt
+++ app/src/main/java/com/alexandre/potentialgrowth/ui/home/HomeActivity.kt
@@ -1,5 +1,7 @@
 package com.alexandre.potentialgrowth.ui.home
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import com.alexandre.potentialgrowth.R
@@ -11,6 +13,7 @@ import com.alexandre.potentialgrowth.ui.mainactivity.MainActivity
 import com.alexandre.potentialgrowth.ui.home.dashboard.DashBoardFragment
 import com.alexandre.potentialgrowth.ui.home.knowledge.KnowledgeFragment
 import com.alexandre.potentialgrowth.ui.home.reward.RewardFragment
+import com.alexandre.potentialgrowth.ui.instruction.InstructionActivity
 
 /**
  * First activity with 3 tabs
@@ -24,9 +27,9 @@ class HomeActivity : MainActivity() {
         setupViewPager(viewPager)
         tabs.setupWithViewPager(viewPager)
         nav_view.menu.getItem(0).isChecked = true
+
+        displayInstructionIfNotLearned()
     }
-
-
 
     private fun setupViewPager(viewPager: ViewPager) {
         val adapter = ViewPagerAdapter(supportFragmentManager)
@@ -34,6 +37,23 @@ class HomeActivity : MainActivity() {
         adapter.addFragment(KnowledgeFragment(), "KNOWLEDGE")
         adapter.addFragment(RewardFragment(), "REWARDS")
         viewPager.adapter = adapter
+    }
+
+    private fun displayInstructionIfNotLearned() {
+        val sharedPref = application.getSharedPreferences(
+                application.getString(R.string.preference_file), Context.MODE_PRIVATE)
+
+        val hasLearned = sharedPref.getBoolean(application.getString(R.string.preference_has_learned_instruction), false)
+
+        if (!hasLearned) {
+            val intent = Intent(this, InstructionActivity::class.java)
+            startActivity(intent)
+
+            with (sharedPref.edit()) {
+                putBoolean(application.getString(R.string.preference_has_learned_instruction), true)
+                apply()
+            }
+        }
     }
 
     internal inner class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
